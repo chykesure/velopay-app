5e4e56b619fbce045fc9906f1ebb4907851c497d
Polycarp Chike Pascal <polycarpchike@gmail.com>
2024-07-13 07:45:09 +0100

    13th July

--- lib/views/Homes/homepage.dart
+++ lib/views/Homes/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // Import to handle SVG assets
 
 void main() {
   runApp(MyApp());
@@ -35,14 +34,21 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _navigateToPaymentPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PaymentPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set the Scaffold background color to white
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.orange,
         elevation: 0,
-        automaticallyImplyLeading: false, // This disables the back arrow
+        automaticallyImplyLeading: false,
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -51,160 +57,135 @@ class _HomePageState extends State<HomePage> {
               constraints: BoxConstraints(
                 minHeight: constraints.maxHeight,
               ),
-              child: Padding(
-                padding: EdgeInsets.all(20), // Adjust padding as needed
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          radius: 42, // Adjust size as needed
-                          backgroundImage: AssetImage('images/pas.jpg'), // Path to your image file
-                          onBackgroundImageError: (exception, stackTrace) {
-                            // Handle image loading error
-                          },
-                        ),
-                        SizedBox(width: 10), // Space between avatar and text
-                        Expanded(
-                          child: RichText(
-                            text: TextSpan(
-                              style: TextStyle(
-                                fontSize: 16, // Default base font size, used for smaller text
-                                color: Colors.black, // Default text color
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'Welcome Chyke ',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w900, // Extra bold
-                                    fontSize: 18, // Larger font size for "Welcome ABC"
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: '👋\n\nWhat bill would you like to pay?',
-                                  style: TextStyle(
-                                    fontSize: 16, // Slightly smaller font size for the question
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 70), // Space between the row and the container
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 20), // Equal left and right margins
-                      padding: EdgeInsets.all(20), // Padding inside the container
-                      decoration: BoxDecoration(
-                        color: Colors.orange, // Background color
-                        borderRadius: BorderRadius.circular(10), // Rounded corners
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+              child: IntrinsicHeight(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
-                            'Total Balance',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
+                          CircleAvatar(
+                            radius: 42,
+                            backgroundImage: AssetImage('images/pas.jpg'),
+                            onBackgroundImageError: (exception, stackTrace) {},
                           ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'NGN ',
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: RichText(
+                              text: TextSpan(
                                 style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.black,
                                 ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'Welcome Chyke ',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '👋\n\nWhat bill would you like to pay?',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                _isBalanceVisible ? '70,000,000.00' : '***',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(width: 10),
-                              IconButton(
-                                icon: Icon(
-                                  _isBalanceVisible ? Icons.visibility : Icons.visibility_off,
-                                  color: Colors.white,
-                                ),
-                                onPressed: _toggleVisibility,
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          ElevatedButton.icon(
-                            onPressed: () {
-                              // Action to fund wallet
-                            },
-                            icon: Icon(Icons.add, color: Colors.orange),
-                            label: Text('Fund Wallet'),
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.orange, backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                    Spacer(), // Fills the remaining space, ensuring flexible height management
-                    SizedBox(height: 100), // Adjust to give space for the bottom navigation bar
-                  ],
+                      SizedBox(height: 70),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Total Balance',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'NGN ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  _isBalanceVisible ? '70,000,000.00' : '***',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                IconButton(
+                                  icon: Icon(
+                                    _isBalanceVisible ? Icons.visibility : Icons.visibility_off,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: _toggleVisibility,
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton.icon(
+                              onPressed: _navigateToPaymentPage, // Navigate to PaymentPage
+                              icon: Icon(Icons.add, color: Colors.orange),
+                              label: Text('Fund Wallet'),
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.orange, backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 100), // Adjust to give space for the bottom navigation bar
+                    ],
+                  ),
                 ),
               ),
             ),
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Action for FAB
-        },
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class PaymentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment Page'),
         backgroundColor: Colors.orange,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8.0,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Services',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_wallet),
-              label: 'Wallet',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.orange,
-          unselectedItemColor: Colors.grey,
-          onTap: _onItemTapped,
-        ),
+      body: Center(
+        child: Text('This is the Payment Page'),
       ),
     );
   }
